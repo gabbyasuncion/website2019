f552b3a88b52dbcde6cc30237d721337d220c86b
gabbyasuncion <35894864+gabbyasuncion@users.noreply.github.com>
2019-08-06 22:13:39 -0700

    Updates

---
+++ precache-manifest.1fc3995c87723a286ea632283f647a1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953c53ea2fab4cf11c3908e5b2a379d9",
+    "url": "/website2019/index.html"
+  },
+  {
+    "revision": "033480fd3ff9d0766697",
+    "url": "/website2019/static/css/main.fe675615.chunk.css"
+  },
+  {
+    "revision": "a0ac6005774159d8f660",
+    "url": "/website2019/static/js/2.b563bf83.chunk.js"
+  },
+  {
+    "revision": "033480fd3ff9d0766697",
+    "url": "/website2019/static/js/main.6299d85d.chunk.js"
+  },
+  {
+    "revision": "e1de216982c03aac3787",
+    "url": "/website2019/static/js/runtime~main.8359a08e.js"
+  }
+]);
